ab47c76cae4af5a5ddec5d752d82227b1fe4ca9b
Zogaj Florent <florent.zogaj@studentfr.ch>
2023-11-14 16:56:45 +0100

    js scrolltrigger

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,75 @@
-import gsap from "gsap";
+// import gsap from "gsap";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.to("#card-animation", {
-  x: 700,
-  y: 280,
-  scrollTrigger: {
-    trigger: "card-section",
-    start: "top top",
-    end: "top=+5000",
-    scrub: true,
-  },
+// const tmln = gsap.timeline();
+
+// gsap.to("#card-animation", {
+//   scrollTrigger: {
+//     trigger: "card-section",
+//     start: "top top",
+//     end: "top=+5000",
+//     scrub: 2,
+//   },
+// });
+
+// tmln.set("#card-animation", {
+//   x: 0,
+//   y: 0,
+//   scrollTrigger: {
+//     trigger: "card-section",
+//     start: "top top",
+//     end: "top=+5000",
+//     scrub: 2,
+//   },
+// });
+
+// tmln.to("#card-animation", {
+//   x: 120,
+//   y: 200,
+//   scrollTrigger: {
+//     trigger: "card-section",
+//     start: "top top",
+//     end: "top=+5000",
+//     scrub: 2,
+//   },
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const cardAnimation = document.getElementById("card-animation");
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".card-section",
+      start: "top top",
+      end: "top+=5000",
+      scrub: 1,
+      markers: true,
+      pin: true,
+    },
+  });
+
+  tl.to(cardAnimation, {
+    duration: 1,
+    scale: 2,
+    x: 200,
+    y: 200,
+    ease: "power2.out",
+  })
+    .to(cardAnimation, {
+      duration: 1,
+      scale: 2,
+      x: 800,
+      y: 200,
+      ease: "power2.out",
+    })
+    .to(cardAnimation, {
+      duration: 1,
+      scale: 2,
+      x: 300,
+      y: 200,
+      ease: "power2.out",
+    });
 });
